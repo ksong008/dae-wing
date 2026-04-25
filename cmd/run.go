0bb65ee1ff0de4aa6fcb06cae1992662f75ad18c
ksong008 <ksong008@gmail.com>
2026-04-25 13:56:46 +0800

    Revert "fix(reload): restore startup run transaction"
    This reverts commit ec0e7ab0e51a64faf8b72364c9775a1467ea9ba9.

--- cmd/run.go
+++ cmd/run.go
@@ -167,10 +167,8 @@ func restoreRunningState() (err error) {
 	if !reload {
 		return nil
 	}
-	tx := db.BeginTx(context.TODO())
 	// Reload.
-	if _, err = config.Run(tx, false); err != nil {
-		tx.Rollback()
+	if _, err = config.Run(context.TODO(), false); err != nil {
 		// Another tx.
 		// Set running = false.
 		tx2 := db.BeginTx(context.TODO())
@@ -188,7 +186,6 @@ func restoreRunningState() (err error) {
 		tx2.Commit()
 		return err
 	}
-	tx.Commit()
 	return nil
 }
 
